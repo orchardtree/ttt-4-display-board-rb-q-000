5ad7e2e1e3c5f77fd5251ebd1973662ecffe953c
Jesse Wenger <wengerj@gmail.com>
2021-02-21 21:01:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,8 @@
 board = ["","","","","","","","",""]
 def display_board (board)
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
